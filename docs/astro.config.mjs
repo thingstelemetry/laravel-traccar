1d36543cdb12f2f18950c12a06de4f69ab59f24c
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2025-10-05 23:48:30 +0300

    Initial commit

---
+++ docs/astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Traccar API Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/tracktelemetry/traccar-api' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
