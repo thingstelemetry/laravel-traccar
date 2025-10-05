4f70fbcc4393f7931efc292933ef3adc3600e3e0
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2025-10-05 23:59:30 +0300

    Add tailwindcss style

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,24 +2,33 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Traccar API Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/tracktelemetry/traccar-api' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Traccar API Docs',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/tracktelemetry/traccar-api' }],
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      { label: 'Example Guide', slug: 'guides/example' },
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+          customCss: [
+              './src/styles/global.css',
+          ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
