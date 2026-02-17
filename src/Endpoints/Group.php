e3dfa86a50c655838466f29dc3d7c227782cdf26
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2026-02-18 01:27:46 +0300

    feat: add Group facade and endpoint with CRUD operations

---
+++ src/Endpoints/Group.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThingsTelemetry\Traccar\Endpoints;
+
+use Illuminate\Support\Collection;
+use ThingsTelemetry\Traccar\Traccar;
+use ThingsTelemetry\Traccar\Dto\GroupData;
+use ThingsTelemetry\Traccar\Dto\StatusData;
+use ThingsTelemetry\Traccar\Requests\Group\GetGroup;
+use ThingsTelemetry\Traccar\Requests\Group\CreateGroup;
+use ThingsTelemetry\Traccar\Requests\Group\DeleteGroup;
+use ThingsTelemetry\Traccar\Requests\Group\UpdateGroup;
+use ThingsTelemetry\Traccar\Requests\Group\GetAllGroups;
+
+class Group extends Traccar
+{
+    /** @throws \Saloon\Exceptions\SaloonException */
+    public function getAll(?bool $all = null, ?int $userId = null, ?bool $excludeAttributes = null): Collection
+    {
+        $response = $this->connector->send(
+            request: new GetAllGroups(
+                all: $all,
+                userId: $userId,
+                excludeAttributes: $excludeAttributes
+            )
+        );
+
+        return $response->dtoOrFail();
+    }
+
+    /** @throws \Saloon\Exceptions\SaloonException */
+    public function get(int $id): GroupData
+    {
+        $response = $this->connector->send(request: new GetGroup(id: $id));
+
+        return $response->dtoOrFail();
+    }
+
+    /** @throws \Saloon\Exceptions\SaloonException */
+    public function create(GroupData $data): GroupData
+    {
+        $response = $this->connector->send(request: new CreateGroup(data: $data));
+
+        return $response->dtoOrFail();
+    }
+
+    /** @throws \Saloon\Exceptions\SaloonException */
+    public function update(GroupData $data): GroupData
+    {
+        $response = $this->connector->send(request: new UpdateGroup(data: $data));
+
+        return $response->dtoOrFail();
+    }
+
+    /** @throws \Saloon\Exceptions\SaloonException */
+    public function delete(int $id): StatusData
+    {
+        $response = $this->connector->send(request: new DeleteGroup(id: $id));
+
+        return $response->dtoOrFail();
+    }
+}
